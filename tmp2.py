89a09c633b252b456da416c7216d06e9fadb6430
Luke Mason <s3630120@student.rmit.edu.au>
2023-10-05 04:22:18 +1100

    sync

--- tmp2.py
+++ tmp2.py
@@ -18,16 +18,17 @@ file_path = root_path + 'train_wkt_v4.csv/train_wkt_v4.csv'
 df = pd.read_csv(file_path)
 ids = df['ImageId'].unique().tolist()
 
-for bnd in range(num_bands):
-    data_list = []
-    for img_id in ids:
-        print(img_id)
-        path = dataset_path + f'{img_id}_interp_4.tif'
-        if path is None or not os.path.exists(path):
-            print(f"Could not find file for image_id: {img_id}")
-            continue
-        with rasterio.open(path) as src:
-            data = src.read(bnd + 1)
-            data_list.append(data)
+data_list = []
+for img_id in ids:
+    print(img_id)
+    path = dataset_path + f'{img_id}_interp_4.tif'
+    if path is None or not os.path.exists(path):
+        print(f"Could not find file for image_id: {img_id}")
+        continue
+    with rasterio.open(path) as src:
+        data = src.read()
+        data_list.append(data)
+data_list = np.array(data_list)
 
-    print(f"Std for Band {bnd + 1}: {np.std(np.array(data_list))}")
+for bnd in range(num_bands):
+    print(f"Std for Band {bnd + 1}: {np.std(data_list[:, bnd, :, :])}")
