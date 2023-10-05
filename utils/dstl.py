1a97c5e8e8a29ec8819e85790dc6c5bc3fd75e8e
Luke Mason <s3630120@student.rmit.edu.au>
2023-10-05 23:15:39 +1100

    sync

--- utils/dstl.py
+++ utils/dstl.py
@@ -68,7 +68,7 @@ def mask_for_polygons(
     """ Return numpy mask for given polygons.
     polygons should already be converted to image coordinates.
     """
-    img_mask = np.zeros(im_size, np.uint8)
+    img_mask = np.full(im_size, np.uint8)
     if not polygons:
         return img_mask
     int_coords = lambda x: np.array(x).round().astype(np.int32)
