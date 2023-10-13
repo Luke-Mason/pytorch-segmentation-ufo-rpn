feccaed50f22ff91f4a43fe662f1fc560121cbf2
Luke Mason <s3630120@student.rmit.edu.au>
2023-10-13 18:39:25 +1100

    sync

--- trainers/trainer.py
+++ trainers/trainer.py
@@ -158,7 +158,8 @@ class Trainer(BaseTrainer):
 
                 # PRINT INFO
                 pixAcc, mIoU, _ = self._get_seg_metrics().values()
-                description = 'EVAL ({}) | Loss: {:.3f}, PixelAcc: {:.2f}, Mean IoU: {:.2f} |'.format( epoch,
+                description = ('EVAL ({}) | Loss: {:.3f}, PixelAcc: {:.3f}, '
+                               'Mean IoU: {:.3f} |').format( epoch,
                                                 self.total_loss.average,
                                                 pixAcc, mIoU)
                 tbar.set_description(description)
@@ -220,8 +221,10 @@ class Trainer(BaseTrainer):
         IoU = 1.0 * self.total_inter / (np.spacing(1) + self.total_union)
         print('IoU: ', IoU)
         mIoU = IoU.mean()
+        print('mIoU: ', mIoU)
+
         return {
-            "Pixel_Accuracy": np.round(pixAcc, 3),
-            "Mean_IoU": np.round(mIoU, 3),
-            "Class_IoU": dict(zip(range(self.num_classes), np.round(IoU, 3)))
+            "pixAcc": np.round(pixAcc, 3),
+            "mIoU": np.round(mIoU, 3),
+            "cIoU": dict(zip(range(self.num_classes), np.round(IoU, 3)))
         }
