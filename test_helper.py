62d93e56224e067262f0ac2d34e8818d3315d11d
Luke Mason <s3630120@student.rmit.edu.au>
2023-10-18 19:24:40 +1100

    sync

--- test_helper.py
+++ test_helper.py
@@ -1,7 +1,7 @@
 
 restricted = False
 def dataset_gateway(arr):
-    return arr[:2] if restricted else arr[:5]
+    return arr[:2] if restricted else arr
 
 def epoch_gateway(epochs):
     return 2 if restricted else epochs
