0a26fc9508c635b9408982a9890efa1dfb46e3f7
Luke Mason <s3630120@gpuserver1head.csit.local>
2023-10-23 09:01:03 +1100

    sync

--- run_experiment.sh
+++ run_experiment.sh
@@ -7,14 +7,7 @@ if [ $# -ne 1 ]; then
 fi
 
 # Extract the <number> from the argument
-arg=$1
-number=$(echo $arg | sed 's/\([0-9]\)/\1/')
-
-# Check if the argument matches the expected format
-if [[ ! $arg =~ ^[0-9]+$ ]]; then
-    echo "Invalid argument format. Expected a digit."
-    exit 1
-fi
+number=$1
 
 # Loop from 0 to 9 (inclusive) and call the Python script
 for i in {0..9}; do
